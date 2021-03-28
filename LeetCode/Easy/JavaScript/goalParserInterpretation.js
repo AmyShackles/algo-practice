23563473599224091a0b5a4066822ee12ea1b350
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:22:54 -0700

    Adds solution for Goal Parser Interpretation

--- LeetCode/Easy/JavaScript/goalParserInterpretation.js
+++ LeetCode/Easy/JavaScript/goalParserInterpretation.js
@@ -1,7 +1,29 @@
 /*
     Source: https://leetcode.com/problems/goal-parser-interpretation/
 */
+// I love regex so like coming up with solutions using it, but realize that it might be an easy way out in an interview
+// function interpret(command) {
+//     return command.replace(/\(\)/g, "o").replace(/\(al\)/g, "al")
+// }
 
-function interpret (command) {};
+function interpret(command) {
+    let interpreted = "";
+    let index = 0;
+    while (index < command.length) {
+        if (command[index] === "G") {
+            interpreted += "G";
+        } else if (command[index] === "(") {
+            if (command[index + 1] === ")") {
+                interpreted += "o";
+                index++;
+            } else if (command.slice(index, index + 4) === "(al)") {
+                interpreted += "al";
+                index += 3;
+            }
+        }
+        index++;
+    }
+    return interpreted;
+}
 
 module.exports = { interpret };
--- LeetCode/Easy/JavaScript/tests/goalParserInterpretation.test.js
+++ LeetCode/Easy/JavaScript/tests/goalParserInterpretation.test.js
@@ -4,35 +4,35 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("interpret", () => {
+describe("interpret", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: command = "G()(al)"
             Output: "Goal"
             */
-            const expected = "Goal";
-            const actual = interpret("G()(al)");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = "Goal";
+        const actual = interpret("G()(al)");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: command = "G()()()()(al)"
             Output: "Gooooal"
             */
-            const expected = "Gooooal";
-            const actual = interpret("G()()()()(al)");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = "Gooooal";
+        const actual = interpret("G()()()()(al)");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: command = "(al)G(al)()()G"
             Output: "alGalooG"
             */
-            const expected = "alGalooG";
-            const actual = interpret("(al)G(al)()()G");
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = "alGalooG";
+        const actual = interpret("(al)G(al)()()G");
+        expect(actual).to.deep.equal(expected);
     });
+});
