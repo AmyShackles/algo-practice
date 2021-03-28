3597012854c3288d1792c22651baba8688b60a19
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:02:19 -0700

    Adding solution for Decode XORed Array

--- LeetCode/Easy/JavaScript/decodeXoredArray.js
+++ LeetCode/Easy/JavaScript/decodeXoredArray.js
@@ -2,6 +2,12 @@
     Source: https://leetcode.com/problems/decode-xored-array/
 */
 
-function decode (encoded,first) {};
+function decode(encoded, first) {
+    let decoded = [first];
+    for (let i = 0; i < encoded.length; i++) {
+        decoded.push(decoded[decoded.length - 1] ^ encoded[i]);
+    }
+    return decoded;
+}
 
 module.exports = { decode };
