266e253840592b4e93693b9973715c91eae97aa6
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:18:14 -0700

    Adding solution for Number of Good Pairs

--- LeetCode/Easy/JavaScript/numberOfGoodPairs.js
+++ LeetCode/Easy/JavaScript/numberOfGoodPairs.js
@@ -2,6 +2,43 @@
     Source: https://leetcode.com/problems/number-of-good-pairs/
 */
 
-function numIdenticalPairs (nums) {};
+// function numIdenticalPairs(nums) {
+//     let count = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] === nums[j]) {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// function numIdenticalPairs(nums) {
+//     let counts = {};
+//     let count = 0;
+//     nums.forEach(val => {
+//         if (counts[val] !== undefined) {
+//             counts[val]++;
+//             count += counts[val]
+//         } else {
+//             counts[val] = 0;
+//         }
+//     })
+//     return count;
+// }
+
+function numIdenticalPairs(nums) {
+    let counts = {};
+    let count = 0;
+    nums.forEach(val => {
+        if (counts[val] !== undefined) {
+            count += counts[val]++;
+        } else {
+            counts[val] = 1;
+        }
+    })
+    return count;
+}
 
 module.exports = { numIdenticalPairs };
--- LeetCode/Easy/JavaScript/tests/numberOfGoodPairs.test.js
+++ LeetCode/Easy/JavaScript/tests/numberOfGoodPairs.test.js
@@ -4,35 +4,35 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("numIdenticalPairs", () => {
+describe("numIdenticalPairs", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: nums = [1,2,3,1,1,3]
             Output: 4
             */
-            const expected = 4;
-            const actual = numIdenticalPairs([1,2,3,1,1,3]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = 4;
+        const actual = numIdenticalPairs([1, 2, 3, 1, 1, 3]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: nums = [1,1,1,1]
             Output: 6
             */
-            const expected = 6;
-            const actual = numIdenticalPairs([1,1,1,1]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = 6;
+        const actual = numIdenticalPairs([1, 1, 1, 1]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: nums = [1,2,3]
             Output: 0
             */
-            const expected = 0;
-            const actual = numIdenticalPairs([1,2,3]);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = 0;
+        const actual = numIdenticalPairs([1, 2, 3]);
+        expect(actual).to.deep.equal(expected);
     });
+});
