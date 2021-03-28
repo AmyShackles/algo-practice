f3d0d6afabb0e192289cbe47dbac710c7aa5f7ce
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:00:14 -0700

    Adding a solution for romanToInteger

--- LeetCode/Easy/JavaScript/romanToInteger.js
+++ LeetCode/Easy/JavaScript/romanToInteger.js
@@ -2,6 +2,55 @@
     Source: https://leetcode.com/problems/roman-to-integer/
 */
 
-function romanToInt (s) {};
+/*
+    Source: https://leetcode.com/problems/roman-to-integer/
+
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+
+    Edge cases:
+    IV            4
+    IX            9
+    VX - uh, no, idiot.  What the fuck, self.
+    XL            40
+    XC            90
+    CD            400
+    CM            900
+
+*/
+
+function romanToInt(s) {
+    const lookup = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    let num = 0;
+    for (let i = 0; i <= s.length - 1; i++) {
+        if (s[i] === "I" && (s[i + 1] === "V" || s[i + 1] === "X")) {
+            num += lookup[s[i + 1]] - lookup[s[i]];
+            i++;
+        } else if (s[i] === "X" && (s[i + 1] === "L" || s[i + 1] === "C")) {
+            num += lookup[s[i + 1]] - lookup[s[i]];
+            i++;
+        } else if (s[i] === "C" && (s[i + 1] === "D" || s[i + 1] === "M")) {
+            num += lookup[s[i + 1]] - lookup[s[i]];
+            i++;
+        } else {
+            num += lookup[s[i]];
+        }
+    }
+    return num;
+}
 
 module.exports = { romanToInt };
--- LeetCode/Easy/JavaScript/tests/romanToInteger.test.js
+++ LeetCode/Easy/JavaScript/tests/romanToInteger.test.js
@@ -4,55 +4,55 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("romanToInt", () => {
+describe("romanToInt", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: s = "III"
             Output: 3
             */
-            const expected = 3;
-            const actual = romanToInt("III");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = 3;
+        const actual = romanToInt("III");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: s = "IV"
             Output: 4
             */
-            const expected = 4;
-            const actual = romanToInt("IV");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = 4;
+        const actual = romanToInt("IV");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: s = "IX"
             Output: 9
             */
-            const expected = 9;
-            const actual = romanToInt("IX");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 4", () => {
-            /*
+        const expected = 9;
+        const actual = romanToInt("IX");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 4", () => {
+        /*
             For sanity checking:
             Input: s = "LVIII"
             Output: 58
             */
-            const expected = 58;
-            const actual = romanToInt("LVIII");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 5", () => {
-            /*
+        const expected = 58;
+        const actual = romanToInt("LVIII");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 5", () => {
+        /*
             For sanity checking:
             Input: s = "MCMXCIV"
             Output: 1994
             */
-            const expected = 1994;
-            const actual = romanToInt("MCMXCIV");
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = 1994;
+        const actual = romanToInt("MCMXCIV");
+        expect(actual).to.deep.equal(expected);
     });
+});
