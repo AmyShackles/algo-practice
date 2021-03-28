a8d3746ae447ee14f3cf4671aa689058a86a7a79
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:13:38 -0700

    Adding solution for Number of Steps to Reduce A Number to Zero

--- LeetCode/Easy/JavaScript/numberOfStepsToReduceANumberToZero.js
+++ LeetCode/Easy/JavaScript/numberOfStepsToReduceANumberToZero.js
@@ -2,6 +2,17 @@
     Source: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 */
 
-function numberOfSteps (num) {};
+function numberOfSteps(num) {
+    let count = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num--;
+        }
+        count++;
+    }
+    return count;
+}
 
 module.exports = { numberOfSteps };
--- LeetCode/Easy/JavaScript/tests/numberOfStepsToReduceANumberToZero.test.js
+++ LeetCode/Easy/JavaScript/tests/numberOfStepsToReduceANumberToZero.test.js
@@ -4,35 +4,35 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("numberOfSteps", () => {
+describe("numberOfSteps", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: num = 14
             Output: 6
             */
-            const expected = 6;
-            const actual = numberOfSteps(14);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = 6;
+        const actual = numberOfSteps(14);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: num = 8
             Output: 4
             */
-            const expected = 4;
-            const actual = numberOfSteps(8);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = 4;
+        const actual = numberOfSteps(8);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: num = 123
             Output: 12
             */
-            const expected = 12;
-            const actual = numberOfSteps(123);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = 12;
+        const actual = numberOfSteps(123);
+        expect(actual).to.deep.equal(expected);
     });
+});
