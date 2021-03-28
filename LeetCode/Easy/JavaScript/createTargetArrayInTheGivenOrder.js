ed27d997bb92a98e8a4ccb422433b2b98e6aac0b
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:49:26 -0700

    Adds solution for Create Target Array in the Given Order

--- LeetCode/Easy/JavaScript/createTargetArrayInTheGivenOrder.js
+++ LeetCode/Easy/JavaScript/createTargetArrayInTheGivenOrder.js
@@ -2,6 +2,12 @@
     Source: https://leetcode.com/problems/create-target-array-in-the-given-order/
 */
 
-function createTargetArray (nums,index) {};
+function createTargetArray (nums,index) {
+    let targetArray = [];
+    nums.forEach((val, i) => {
+        targetArray.splice(index[i], 0, val)
+    });
+    return targetArray;
+};
 
 module.exports = { createTargetArray };
--- LeetCode/Easy/JavaScript/tests/createTargetArrayInTheGivenOrder.test.js
+++ LeetCode/Easy/JavaScript/tests/createTargetArrayInTheGivenOrder.test.js
@@ -4,35 +4,35 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("createTargetArray", () => {
+describe("createTargetArray", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
             Output: [0,4,1,3,2]
             */
-            const expected = [0,4,1,3,2];
-            const actual = createTargetArray([0,1,2,3,4],[0,1,2,2,1]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = [0, 4, 1, 3, 2];
+        const actual = createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
             Output: [0,1,2,3,4]
             */
-            const expected = [0,1,2,3,4];
-            const actual = createTargetArray([1,2,3,4,0],[0,1,2,3,0]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = [0, 1, 2, 3, 4];
+        const actual = createTargetArray([1, 2, 3, 4, 0], [0, 1, 2, 3, 0]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: nums = [1], index = [0]
             Output: [1]
             */
-            const expected = [1];
-            const actual = createTargetArray([1],[0]);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = [1];
+        const actual = createTargetArray([1], [0]);
+        expect(actual).to.deep.equal(expected);
     });
+});
