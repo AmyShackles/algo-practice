7da0dae46d8ec5873bbe40d37ce665989b2716fa
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:08:56 -0700

    Adding solution for Shuffle String

--- LeetCode/Easy/JavaScript/shuffleString.js
+++ LeetCode/Easy/JavaScript/shuffleString.js
@@ -2,6 +2,22 @@
     Source: https://leetcode.com/problems/shuffle-string/
 */
 
-function restoreString (s,indices) {};
+// Adds overhead of creating an array and joining it at the end
+// function restoreString(s, indices) {
+//     let arr = [];
+//     for (let i = 0; i < s.length; i++) {
+//         arr[indices[i]] = s[i];
+//     }
+//     return arr.join("")
+// }
+
+function restoreString(s, indices) {
+    let str = ""
+    for (let i = 0; i < s.length; i++) {
+        str += s[indices.indexOf(i)]
+    }
+    return str;
+}
+
 
 module.exports = { restoreString };
--- LeetCode/Easy/JavaScript/tests/shuffleString.test.js
+++ LeetCode/Easy/JavaScript/tests/shuffleString.test.js
@@ -4,55 +4,55 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("restoreString", () => {
+describe("restoreString", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
             Output: "leetcode"
             */
-            const expected = "leetcode";
-            const actual = restoreString("codeleet",[4,5,6,7,0,2,1,3]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = "leetcode";
+        const actual = restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: s = "abc", indices = [0,1,2]
             Output: "abc"
             */
-            const expected = "abc";
-            const actual = restoreString("abc",[0,1,2]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = "abc";
+        const actual = restoreString("abc", [0, 1, 2]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: s = "aiohn", indices = [3,1,4,2,0]
             Output: "nihao"
             */
-            const expected = "nihao";
-            const actual = restoreString("aiohn",[3,1,4,2,0]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 4", () => {
-            /*
+        const expected = "nihao";
+        const actual = restoreString("aiohn", [3, 1, 4, 2, 0]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 4", () => {
+        /*
             For sanity checking:
             Input: s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
             Output: "arigatou"
             */
-            const expected = "arigatou";
-            const actual = restoreString("aaiougrt",[4,0,2,6,7,3,1,5]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 5", () => {
-            /*
+        const expected = "arigatou";
+        const actual = restoreString("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 5", () => {
+        /*
             For sanity checking:
             Input: s = "art", indices = [1,0,2]
             Output: "rat"
             */
-            const expected = "rat";
-            const actual = restoreString("art",[1,0,2]);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = "rat";
+        const actual = restoreString("art", [1, 0, 2]);
+        expect(actual).to.deep.equal(expected);
     });
+});
