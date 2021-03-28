91bd9683557813e31213d90ca679d529dd83a258
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:14:59 -0700

    Adding solution for Jewels and Stones

--- LeetCode/Easy/JavaScript/jewelsAndStones.js
+++ LeetCode/Easy/JavaScript/jewelsAndStones.js
@@ -2,6 +2,8 @@
     Source: https://leetcode.com/problems/jewels-and-stones/
 */
 
-function numJewelsInStones (jewels,stones) {};
+function numJewelsInStones(jewels, stones) {
+    return [...stones].filter((val) => jewels.includes(val)).length;
+}
 
 module.exports = { numJewelsInStones };
--- LeetCode/Easy/JavaScript/tests/jewelsAndStones.test.js
+++ LeetCode/Easy/JavaScript/tests/jewelsAndStones.test.js
@@ -4,25 +4,25 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("numJewelsInStones", () => {
+describe("numJewelsInStones", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: jewels = "aA", stones = "aAAbbbb"
             Output: 3
             */
-            const expected = 3;
-            const actual = numJewelsInStones("aA","aAAbbbb");
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = 3;
+        const actual = numJewelsInStones("aA", "aAAbbbb");
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: jewels = "z", stones = "ZZ"
             Output: 0
             */
-            const expected = 0;
-            const actual = numJewelsInStones("z","ZZ");
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = 0;
+        const actual = numJewelsInStones("z", "ZZ");
+        expect(actual).to.deep.equal(expected);
     });
+});
