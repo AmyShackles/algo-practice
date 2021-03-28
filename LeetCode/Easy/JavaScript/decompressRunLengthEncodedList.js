457cca1f1a1913b25f7cf4ac9616d7b596e67e04
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:03:40 -0700

    Adding solution for Decompress Run Length Encoded List

--- LeetCode/Easy/JavaScript/decompressRunLengthEncodedList.js
+++ LeetCode/Easy/JavaScript/decompressRunLengthEncodedList.js
@@ -2,6 +2,12 @@
     Source: https://leetcode.com/problems/decompress-run-length-encoded-list/
 */
 
-function decompressRLElist (nums) {};
+function decompressRLElist(nums) {
+    let arr = [];
+    for (let i = 0; i < nums.length - 1; i += 2) {
+        arr.push(...new Array(nums[i]).fill(nums[i + 1]));
+    }
+    return arr;
+}
 
 module.exports = { decompressRLElist };
--- LeetCode/Easy/JavaScript/tests/decompressRunLengthEncodedList.test.js
+++ LeetCode/Easy/JavaScript/tests/decompressRunLengthEncodedList.test.js
@@ -4,25 +4,25 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("decompressRLElist", () => {
+describe("decompressRLElist", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: nums = [1,2,3,4]
             Output: [2,4,4,4]
             */
-            const expected = [2,4,4,4];
-            const actual = decompressRLElist([1,2,3,4]);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = [2, 4, 4, 4];
+        const actual = decompressRLElist([1, 2, 3, 4]);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: nums = [1,1,2,3]
             Output: [1,3,3]
             */
-            const expected = [1,3,3];
-            const actual = decompressRLElist([1,1,2,3]);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = [1, 3, 3];
+        const actual = decompressRLElist([1, 1, 2, 3]);
+        expect(actual).to.deep.equal(expected);
     });
+});
