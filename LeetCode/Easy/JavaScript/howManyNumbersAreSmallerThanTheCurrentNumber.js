60fcef5b1c44b44918ec97577ce8d639f2cc21d1
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:20:41 -0700

    Adds solution for How Many Numbers Are Smaller Than The Current Number

--- LeetCode/Easy/JavaScript/howManyNumbersAreSmallerThanTheCurrentNumber.js
+++ LeetCode/Easy/JavaScript/howManyNumbersAreSmallerThanTheCurrentNumber.js
@@ -2,6 +2,26 @@
     Source: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
 */
 
-function smallerNumbersThanCurrent (nums) {};
+// function smallerNumbersThanCurrent(nums) {
+//     let arr = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         let current = 0;
+//         for (let j = 0; j < nums.length; j++) {
+//             if (i === j) {
+//                 continue;
+//             }
+//             if (nums[j] < nums[i]) {
+//                 current++;
+//             }
+//         }
+//         arr[i] = current;
+//     }
+//     return arr;
+// }
+
+function smallerNumbersThanCurrent(nums) {
+    let sorted = [...nums].sort((a, b) => a - b);
+    return nums.map(num => sorted.indexOf(num))
+}
 
 module.exports = { smallerNumbersThanCurrent };
