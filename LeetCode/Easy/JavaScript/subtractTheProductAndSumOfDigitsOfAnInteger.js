7ae75a2836c932354a27122ee8b3265bad327e5f
Amy Shackles <amyshackles@gmail.com>
2021-03-28 15:06:50 -0700

    Adding answer for Subtract the Produce and Sume of Digits Of An Integer

--- LeetCode/Easy/JavaScript/subtractTheProductAndSumOfDigitsOfAnInteger.js
+++ LeetCode/Easy/JavaScript/subtractTheProductAndSumOfDigitsOfAnInteger.js
@@ -2,6 +2,27 @@
     Source: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 */
 
-function subtractProductAndSum (n) {};
+// First solution -- manipulates number into a string
+// function subtractProductAndSum(n) {
+//     let product = 1, sum = 0;
+//     let numString = n.toString()
+//     for (let i = 0; i < numString.length; i++) {
+//         product *= numString[i];
+//         sum += +numString[i];
+//     };
+//     return product - sum;
+// }
+
+// Second solution, treats the number as a number
+function subtractProductAndSum(n) {
+    let sum = 0, product = 1, modulo = 0;
+    while (n > 0) {
+        mod = n % 10;
+        sum += mod;
+        product *= mod;
+        n = Math.trunc(n / 10)
+    }
+    return product - sum;
+}
 
 module.exports = { subtractProductAndSum };
--- LeetCode/Easy/JavaScript/tests/subtractTheProductAndSumOfDigitsOfAnInteger.test.js
+++ LeetCode/Easy/JavaScript/tests/subtractTheProductAndSumOfDigitsOfAnInteger.test.js
@@ -4,25 +4,25 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("subtractProductAndSum", () => {
+describe("subtractProductAndSum", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: n = 234
             Output: 15 
             */
-            const expected = 15 ;
-            const actual = subtractProductAndSum(234);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = 15;
+        const actual = subtractProductAndSum(234);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: n = 4421
             Output: 21
             */
-            const expected = 21;
-            const actual = subtractProductAndSum(4421);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = 21;
+        const actual = subtractProductAndSum(4421);
+        expect(actual).to.deep.equal(expected);
     });
+});
