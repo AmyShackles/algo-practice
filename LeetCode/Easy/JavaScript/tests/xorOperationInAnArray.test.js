a9928234e05a35ec79c6a9471358658cfb6c0f8c
Amy Shackles <amyshackles@gmail.com>
2021-03-28 17:00:42 -0700

    Adds solution for XOR operation in an array

--- LeetCode/Easy/JavaScript/tests/xorOperationInAnArray.test.js
+++ LeetCode/Easy/JavaScript/tests/xorOperationInAnArray.test.js
@@ -4,45 +4,45 @@ const expect = require("chai").expect;
 describe.todo = function(title, callback) {
     return it.skip("SKIPPED " + title, callback);
 };
-describe.todo("xorOperation", () => {
+describe("xorOperation", () => {
     it("should return the right value for Example 1", () => {
-            /*
+        /*
             For sanity checking:
             Input: n = 5, start = 0
             Output: 8
             */
-            const expected = 8;
-            const actual = xorOperation(5,0);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 2", () => {
-            /*
+        const expected = 8;
+        const actual = xorOperation(5, 0);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 2", () => {
+        /*
             For sanity checking:
             Input: n = 4, start = 3
             Output: 8
             */
-            const expected = 8;
-            const actual = xorOperation(4,3);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 3", () => {
-            /*
+        const expected = 8;
+        const actual = xorOperation(4, 3);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 3", () => {
+        /*
             For sanity checking:
             Input: n = 1, start = 7
             Output: 7
             */
-            const expected = 7;
-            const actual = xorOperation(1,7);
-            expect(actual).to.deep.equal(expected);
-        });
-	it("should return the right value for Example 4", () => {
-            /*
+        const expected = 7;
+        const actual = xorOperation(1, 7);
+        expect(actual).to.deep.equal(expected);
+    });
+    it("should return the right value for Example 4", () => {
+        /*
             For sanity checking:
             Input: n = 10, start = 5
             Output: 2
             */
-            const expected = 2;
-            const actual = xorOperation(10,5);
-            expect(actual).to.deep.equal(expected);
-        });
+        const expected = 2;
+        const actual = xorOperation(10, 5);
+        expect(actual).to.deep.equal(expected);
     });
+});
--- LeetCode/Easy/JavaScript/xorOperationInAnArray.js
+++ LeetCode/Easy/JavaScript/xorOperationInAnArray.js
@@ -2,6 +2,22 @@
     Source: https://leetcode.com/problems/xor-operation-in-an-array/
 */
 
-function xorOperation (n,start) {};
+// Instructions included a directive to create an array,
+// function xorOperation (n,start) {
+//     let arr = [];
+//     for (let i = 0; i < n; i++) {
+//         arr[i] = start + 2 * i;
+//     }
+//     return arr.reduce((prev, current) => prev ^ current);
+// };
+
+// However, an array isn't actually necessary to get to a solution and I don't understand the reasoning for it
+function xorOperation (n,start) {
+    let val = start + 2 * 0;
+    for (let i = 1; i < n; i++) {
+        val ^= start + 2 * i;
+    }
+    return val;
+};
 
 module.exports = { xorOperation };
