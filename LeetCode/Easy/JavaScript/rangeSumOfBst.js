b6f2fec4607fe0b013eec2b524e3acd3a6523d1d
Amy Shackles <amyshackles@gmail.com>
2021-03-30 11:00:31 -0700

    Adds solution for Range Sum of BST

--- LeetCode/Easy/JavaScript/rangeSumOfBst.js
+++ LeetCode/Easy/JavaScript/rangeSumOfBst.js
@@ -2,6 +2,22 @@
     Source: https://leetcode.com/problems/range-sum-of-bst/
 */
 
-function rangeSumBST (root,low,high) {};
+function rangeSumBST (root,low,high) {
+    let sum = 0;
+    let stack = [root];
+    while (stack.length > 0) {
+        let node = stack.shift();
+        if (node.val < low) {
+            node.right && stack.push(node.right);
+        } else if (node.val > high) {
+            node.left && stack.push(node.left);
+        } else {
+            sum += node.val;
+            node.left && stack.push(node.left);
+            node.right && stack.push(node.right);
+        }
+    }
+    return sum;
+};
 
 module.exports = { rangeSumBST };
