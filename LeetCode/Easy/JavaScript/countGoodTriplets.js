b3a90cd870f8e82f5b3997af063ae76ccc6d2a13
Amy Shackles <amyshackles@gmail.com>
2021-04-13 10:52:07 -0700

    Adding solution for Count Good Triplets

--- LeetCode/Easy/JavaScript/countGoodTriplets.js
+++ LeetCode/Easy/JavaScript/countGoodTriplets.js
@@ -2,6 +2,22 @@
     Source: https://leetcode.com/problems/count-good-triplets/
 */
 
-function countGoodTriplets (arr,a,b,c) {};
+var countGoodTriplets = function (arr, a, b, c) {
+    let goodPairs = 0;
+    for (let i = 0; i < arr.length - 2; i++) {
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            for (let k = j + 1; k < arr.length; k++) {
+                if (
+                    Math.abs(arr[i] - arr[j]) <= a &&
+                    Math.abs(arr[j] - arr[k]) <= b &&
+                    Math.abs(arr[i] - arr[k]) <= c
+                ) {
+                    goodPairs++;
+                }
+            }
+        }
+    }
+    return goodPairs;
+};
 
 module.exports = { countGoodTriplets };
